5b66229e28e77c8f05004834cd39149803aa060b
pesetskyps <root@puppet110.hosting.ad.viacom.com>
2015-05-26 09:30:26 -0400

    editted admin user to use facts

--- environments/production/modules/ps_sql/manifests/sqlexpress.pp
+++ environments/production/modules/ps_sql/manifests/sqlexpress.pp
@@ -6,7 +6,7 @@ class ps_sql::sqlexpress{
 		instancedir    => 'C:\\Program Files\\Microsoft SQL Server',
 		ascollation    => 'Latin1_General_CI_AS',
 		sqlcollation   => 'SQL_Latin1_General_CP1_CI_AS',
-		admin          => '"dsccl1\SQLSVC"',
+		admin          => "$hostname\\SQLSVC",
 		sapwd          => 'Epam_2010',
 		iniFile        => 'ps_sql/ConfigurationFile.ini'
 	}
