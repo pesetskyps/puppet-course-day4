2d5d1f0a67cc885f8b8f57f23849b36f19ad4a4c
root <root@localhost.localdomain>
2015-05-31 15:43:49 -0400

    added role dev webappdb

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -3,10 +3,5 @@ node default {
 
 }
 node 'win-r3sga74n50h' {
-  include profile::dev::base
-  include profile::dev::iis
-  include profile::dev::mysite
-  include profile::dev::myservice        
-  include profile::dev::sqlexpress
-  include profile::dev::create_northwind_database
+  include role::northwind::dev::webappdb
 }
--- environments/production/modules/role/northwind/dev/webappdb.pp
+++ environments/production/modules/role/northwind/dev/webappdb.pp
@@ -1,7 +1,8 @@
 class role::northwind::dev::webappdb inherits role {
+	include profile::dev::base
 	include profile::dev::iis
 	include profile::dev::mysite
-	include profile::dev::myservice
+	include profile::dev::myservice        
 	include profile::dev::sqlexpress
-	include profile::dev::northwinddb
+	include profile::dev::create_northwind_database
 }
