7dd6f4932a49b1dcde825c560ebf0657a1b59843
root <root@localhost.localdomain>
2015-06-09 06:06:41 -0400

    start to work on m4_examples

--- environments/production/modules/ps_iis/manifests/iis.pp
+++ environments/production/modules/ps_iis/manifests/iis.pp
@@ -1,14 +1,16 @@
-class ps_iis::iis($featuresToInclude='IIS-WebServer', $featuresToExclude=''){
-  #uninstall
-  if($featuresToExclude != ''){
-    dism {$featuresToExclude:
-      ensure  => absent,    
-    }
-  }
+class ps_iis::iis($featuresToInclude, $featuresToExclude){
 
   #install
   dism {$featuresToInclude:
     ensure  => present,
     all     => true,
+  }
+
+  #uninstall
+  if($featuresToExclude != ''){
+    dism {$featuresToExclude:
+      ensure  => absent,
+      require => Dism[$featuresToInclude]   
+    }
   }
 }
